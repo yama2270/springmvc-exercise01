b32edb194ca1c4432e942e029de2793f71caae05
hwyang <qoarhfl@gmail.com>
2023-05-09 16:26:49 +0900

    Initial commit

---
+++ src/main/java/springmvc/exercise01/Exercise01Application.java
@@ -0,0 +1,13 @@
+package springmvc.exercise01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise01Application.class, args);
+	}
+
+}
---
+++ src/test/java/springmvc/exercise01/Exercise01ApplicationTests.java
@@ -0,0 +1,13 @@
+package springmvc.exercise01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
